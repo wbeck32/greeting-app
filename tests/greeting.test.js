3fb63644b004832382d1ffd0adc84b4bd44166ea
Wendy Beck <webeck@gmail.com>
2017-07-15 00:53:44 -0700

    kickstarting travis

--- tests/greeting.test.js
+++ tests/greeting.test.js
@@ -1,12 +1,14 @@
 // mocha test
+// 1. Library with basic math functions: `add`, `subtract`, `multiple`, `divide`.
+// 2. throw an error `throw new Error('your message')` when attempting to divide by 0
 const assert = require('assert');
-const greeter = require('../src/greeter');
+const greeter = require('../src/calculator');
 
 // function greeter(name) {
 //   return `Hello ${name}!`;
 // }
 
-describe('greeting library', () =>{
+describe('greeting library', () => {
   it('greets by name', () => {
     const greeting = greeter('Joe');
     assert.equal(greeting, 'Hello Joe!')
@@ -14,6 +16,5 @@ describe('greeting library', () =>{
 });
 
 it('defaults to stranger when no name is given', () => {
-
 
 })
